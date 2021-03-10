7f5e78357d151574437b5ec61eb954b4d8800a50
Valdis <valdisnn19@yandex.ru>
2021-03-10 22:23:37 +0300

    secondDay

--- scripts/index.js
+++ scripts/index.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", (e) => {
     /*_service-information_*/
 
     const tabs = () => {
-        const cardDetailChangeElem = document.querySelectorAll('.card-detail__change');
+        const cardDetailChangeElems = document.querySelectorAll('.card-detail__change');
         const cardDetailsTitleElem = document.querySelector('.card-details__title');
         const cardImageItem = document.querySelector('.card__image_item');
         const cardDetailPrice = document.querySelector('.card-details__price');
@@ -33,10 +33,10 @@ document.addEventListener("DOMContentLoaded", (e) => {
         ];
 
         const undoActive = () => {
-            cardDetailChangeElem.forEach(btn => btn.classList.remove('active'));
+            cardDetailChangeElems.forEach(btn => btn.classList.remove('active'));
         }
 
-        cardDetailChangeElem.forEach((btn, i) => {
+        cardDetailChangeElems.forEach((btn, i) => {
             btn.addEventListener('click', () => {
                 if (!btn.classList.contains('active')) {
                     undoActive();
@@ -51,5 +51,71 @@ document.addEventListener("DOMContentLoaded", (e) => {
         });
     };
 
+    const accordion = () => {
+        const characteristicsListElem = document.querySelector('.characteristics__list');
+        const characteristicsItemElems = document.querySelectorAll('.characteristics__item');
+
+        characteristicsItemElems.forEach((elem) => {
+            if (elem.children[1].classList.contains('active')) {
+                elem.children[1].style.height = `${elem.children[1].scrollHeight}px`;
+            }
+        })
+
+        const open = (button, dropDown) => {
+            closeAllDrops();
+            dropDown.style.height = `${dropDown.scrollHeight}px`;
+            button.classList.add('active');
+            dropDown.classList.add('active');
+        };
+
+        const close = (button, dropDown) => {
+            button.classList.remove('active');
+            dropDown.classList.remove('active');
+            dropDown.style.height = '';
+        };
+
+        const closeAllDrops = (button, dropDown) => {
+            characteristicsItemElems.forEach((elem) => {
+                if (elem.children[0] !== button && elem.children[1] !== dropDown) {
+                    close(elem.children[0], elem.children[1]);
+                }
+            });
+        };
+
+        characteristicsListElem.addEventListener('click', (event) => {
+            const target = event.target;
+            if (target.classList.contains('characteristics__title')) {
+                const parent = target.closest('.characteristics__item');
+                const description = parent.querySelector('.characteristics__description');
+                description.classList.contains('active') ? close(target, description) : open(target, description);
+            }
+        });
+
+        document.body.addEventListener('click', (event) => {
+            const target = event.target;
+            if (!target.closest('.characteristics__list')) {
+                closeAllDrops();
+            }
+        })
+    };
+
+    const modal = () => {
+        const cardDetailsButtonBuy = document.querySelector('.card-details__button_buy');
+        const modal = document.querySelector('.modal');
+
+        cardDetailsButtonBuy.addEventListener('click', () => {
+            modal.classList.add('open');
+        });
+
+        modal.addEventListener('click', (event) => {
+            const target = event.target;
+            if (target.classList.contains('modal__close') || target.classList.contains('modal')) {
+                modal.classList.remove('open');
+            }
+        });
+    };
+
     tabs();
+    accordion();
+    modal();
 });
