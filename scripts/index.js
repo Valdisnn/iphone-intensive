9639db1eb5bef487dde4d3f646bded5f04ea20e0
Valdis <valdisnn19@yandex.ru>
2021-03-11 21:56:28 +0300

    thirdDay

--- scripts/index.js
+++ scripts/index.js
@@ -4,6 +4,23 @@ document.addEventListener("DOMContentLoaded", (e) => {
     console.log(e.target, 'Страница прогружена !');
     /*_service-information_*/
 
+    const getData = (url, callback) => {
+        const request = new XMLHttpRequest();
+        request.open('GET', url);
+
+        request.addEventListener('readystatechange', () => {
+            if (request.readyState !== 4) return;
+            if (request.status === 200) {
+                const res = JSON.parse(request.response);
+                callback(res);
+            } else {
+                console.error(new Error('Ошибка:' + request.status));
+            }
+        });
+
+        request.send();
+    };
+
     const tabs = () => {
         const cardDetailChangeElems = document.querySelectorAll('.card-detail__change');
         const cardDetailsTitleElem = document.querySelector('.card-details__title');
@@ -25,7 +42,7 @@ document.addEventListener("DOMContentLoaded", (e) => {
                 memoryROM: 256,
             },
             {
-                name: 'Смартфон Apple iPhone 12 Pro 128GB Gold',
+                name: 'Смартфон Apple iPhone 12 Pro 512GB Gold',
                 img: 'img/iPhone-gold.png',
                 price: '129990',
                 memoryROM: 512,
@@ -62,7 +79,7 @@ document.addEventListener("DOMContentLoaded", (e) => {
         })
 
         const open = (button, dropDown) => {
-            closeAllDrops();
+            closeAllDrops(button, dropDown);
             dropDown.style.height = `${dropDown.scrollHeight}px`;
             button.classList.add('active');
             dropDown.classList.add('active');
@@ -96,26 +113,76 @@ document.addEventListener("DOMContentLoaded", (e) => {
             if (!target.closest('.characteristics__list')) {
                 closeAllDrops();
             }
-        })
+        });
     };
 
     const modal = () => {
         const cardDetailsButtonBuy = document.querySelector('.card-details__button_buy');
+        const cardDetailsButtonDelivery = document.querySelector('.card-details__button_delivery');
         const modal = document.querySelector('.modal');
+        const cardDetailsTitle = document.querySelector('.card-details__title');
+        const modalTitle = document.querySelector('.modal__title');
+        const modalSubtitle = modal.querySelector('.modal__subtitle');
+        const modalTitleSubmit = document.querySelector('.modal__title-submit');
 
-        cardDetailsButtonBuy.addEventListener('click', () => {
+        const openModal = (event) => {
+            const target = event.target;
             modal.classList.add('open');
-        });
+            document.addEventListener('keydown', escapeHandler);
+            modalTitle.textContent = cardDetailsTitle.textContent;
+            modalTitleSubmit.value = cardDetailsTitle.textContent;
+            modalSubtitle.textContent = target.dataset.buttonBuy;
+        };
+
+        const closeModal = () => {
+            modal.classList.remove('open');
+            document.removeEventListener('keydown', escapeHandler);
+        };
 
         modal.addEventListener('click', (event) => {
             const target = event.target;
-            if (target.classList.contains('modal__close') || target.classList.contains('modal')) {
+            if (target.classList.contains('modal__close') || target === modal) {
                 modal.classList.remove('open');
             }
         });
+
+        const escapeHandler = (event) => {
+            if (event.code === 'Escape' || event.key === 'Escape' || event.keyCode === 27) {
+                closeModal();
+            }
+        };
+
+        cardDetailsButtonBuy.addEventListener('click', openModal);
+        cardDetailsButtonDelivery.addEventListener('click', openModal);
+    };
+
+    const renderCrossSell = () => {
+        const crossSellList = document.querySelector('.cross-sell__list');
+
+        const createCrossSellItem = (good) => {
+            const liItem = document.createElement('li');
+            liItem.innerHTML = `
+                <article class="cross-sell__item">
+                    <img class="cross-sell__image" src="${good.photo}" alt="${good.name}">
+                    <h3 class="cross-sell__title">${good.name}</h3>
+                    <p class="cross-sell__price">${good.price} ₽</p>
+                    <div class="button button_buy cross-sell__button">Купить</div>
+                </article>
+            `;
+            return liItem;
+        };
+
+        const createCrossSellList = (goods) => {
+            goods.forEach(item => {
+                crossSellList.append(createCrossSellItem(item));
+            });
+        };
+
+        getData('cross-sell-dbase/dbase.json', createCrossSellList);
     };
 
     tabs();
     accordion();
     modal();
+    renderCrossSell();
 });
